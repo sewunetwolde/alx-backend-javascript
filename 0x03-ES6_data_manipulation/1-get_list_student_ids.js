1955d9bbc19f56f891ff5d42df8013735a0d01a8
sewunetwolde <nekodmos@gmail.com>
2023-01-31 09:22:01 +0300

    01

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,17 +1,5 @@
-/**
- * Retrieves ids from a list of students.
- * @param {{
- *   id: Number,
- *   firstName: String,
- *   location: String
- * }[]} students - The list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns
- */
-export default function getListStudentIds(students) {
-  if (students instanceof Array) {
-    return students.map((student) => student.id);
-  }
-  return [];
+// eslint-disable-next-line consistent-return
+export default function getListStudentIds(arg) {
+  if (!Array.isArray(arg)) return [];
+  return arg.map((el) => el.id);
 }
-
